fe13a2e61eb3bc75fc83e5d9144989eef41df031
alexlampe14 <lampealex888@gmail.com>
2024-02-12 01:01:41 -0500

    feat: add sidebar, organize main navbar

---
+++ components/pageContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Children } from "react";
+
+export default function pageContainer({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex flex-col justify-around max-w-7xl mx-auto">
+      {children}
+    </div>
+  );
+}
