6db6b87e1f432e096dcbcb03b0466a21e6684531
lampealex888 <lampealex888@gmail.com>
2024-03-15 19:15:22 -0400

    merge conflicts

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,5 +1,30 @@
-import UnderConstruction from "@/components/under-construction";
+import PageTitle from "@/components/pageTitle";
+import PageContainer from "@/components/container";
+import Sidebar from "@/components/sidebar";
 
-export default function OurMembers() {
-  return <UnderConstruction />;
+export default function History() {
+  return (
+    <div>
+      <PageContainer>
+        <PageTitle title="About Us" />
+
+        <div className="flex">
+          <div className="flex-grow">
+
+            <p style={{ marginBottom: '16px' }}>
+              Example Example Example Example Example Example Example Example Example Example Example Example Example Example
+            </p>
+
+            <p style={{ marginBottom: '16px' }}>
+            Example Example Example Example Example Example Example Example Example Example Example Example Example Example
+            </p>
+
+
+          </div>
+
+          <Sidebar />
+        </div>
+      </PageContainer>
+    </div>
+  );
 }
