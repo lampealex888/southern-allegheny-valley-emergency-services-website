deb34eea553b657a35cf1a7e881cff7531ffe292
lampealex888 <lampealex888@gmail.com>
2024-03-15 19:10:22 -0400

    Update page.tsx

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import PageTitle from "@/components/pageTitle";
 import PageContainer from "@/components/pageContainer";
 import Sidebar from "@/components/sidebar";
